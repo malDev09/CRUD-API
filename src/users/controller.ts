249497a3a8f18e5284442a1e047b8e9544e09bc0
malDev09 <159049731+malDev09@users.noreply.github.com>
2024-02-09 16:39:50 +0300

    feat: add delete & update endpoints

--- src/users/controller.ts
+++ src/users/controller.ts
@@ -1,4 +1,5 @@
 import { IncomingMessage, ServerResponse } from 'http';
+import { sendErrorResponse, sendSuccessResponse, uuidRegex } from '../helpers';
 import { UsersService } from "./service";
 
 
@@ -7,34 +8,96 @@ export class UsersController {
 
 
   async getAllUsers  (req: IncomingMessage, res: ServerResponse){
-    const users = await this.usersService.getAll();
-    res.end(JSON.stringify(users));
+    try {
+      const users = await this.usersService.getAll();
+      sendSuccessResponse(res, 200, users)
+
+  } catch (error) {
+      sendErrorResponse(res, 500, 'Internal Server Error');
+  }
   };
 
-  async getUserById  (req: IncomingMessage, res: ServerResponse){
+  async getUserById(req: IncomingMessage, res: ServerResponse) {
     if (!req.url) {
-      res.writeHead(400, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ message: 'URL is not defined' }));
+      sendErrorResponse(res, 400, 'URL is not defined ');
+      return;
+    }
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const userId = url.pathname.substring(url.pathname.lastIndexOf('/') + 1);
+    if (!userId || !uuidRegex.test(userId) ) {
+      sendErrorResponse(res, 400, 'Invalid userId');
       return;
   }
-  const url = new URL(req.url, `http://${req.headers.host}`);
+    try {
+        const user = await this.usersService.getUserById(userId);
+        sendSuccessResponse(res, 200, user)
+    } catch (error) {
+      sendErrorResponse(res, 404, 'User not found');
+    }
+}
 
-  const userId = url.pathname.split('/').pop();
-    const user = await this.usersService.getUserById(userId)
-    res.end(JSON.stringify(user))
-  }
 
- async createUser (req: IncomingMessage, res: ServerResponse) {
+  async createUser(req: IncomingMessage, res: ServerResponse) {
     let body = '';
+
     req.on('data', chunk => {
         body += chunk.toString();
     });
-    req.on('end', () => {
-        let newUser: User = JSON.parse(body);
-        users.push(newUser);
-        res.writeHead(201, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(newUser));
+
+    req.on('end', async () => {
+        try {
+            const newUser: User = JSON.parse(body);
+            const createdUser = await this.usersService.createUser(newUser);
+            
+            res.writeHead(201, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(createdUser));
+        } catch (error) {
+          sendErrorResponse(res, 400, 'Invalid request body');
+        }
     });
-};
+}
+async updateUser(req: IncomingMessage, res: ServerResponse) {
+  if (!req.url) {
+      sendErrorResponse(res, 400, 'URL is not defined');
+      return;
+  }
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const userId = url.pathname.substring(url.pathname.lastIndexOf('/') + 1);
+
+  if (!userId || !uuidRegex.test(userId)) {
+      sendErrorResponse(res, 400, 'Invalid userId');
+      return;
+  }
+
+  try {
+      const updatedUser = await this.usersService.updateUser(userId, req);
+      sendSuccessResponse(res, 200, updatedUser);
+  } catch (error) {
+      sendErrorResponse(res, 404, 'User not found');
+  }
+}
+
+async deleteUser(req: IncomingMessage, res: ServerResponse) {
+  if (!req.url) {
+      sendErrorResponse(res, 400, 'URL is not defined');
+      return;
+  }
+
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const userId = url.pathname.substring(url.pathname.lastIndexOf('/') + 1);
+
+  if (!userId || !uuidRegex.test(userId)) {
+      sendErrorResponse(res, 400, 'Invalid userId');
+      return;
+  }
+
+  try {
+      await this.usersService.deleteUser(userId);
+      sendSuccessResponse(res, 204);
+  } catch (error) {
+      sendErrorResponse(res, 404, 'User not found');
+  }
+}
 
 }
+
