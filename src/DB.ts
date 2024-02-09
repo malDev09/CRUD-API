272395ba9012c6a8cf7be2ed01f6d537982bdc5f
malDev09 <159049731+malDev09@users.noreply.github.com>
2024-02-09 16:23:35 +0300

    feat: Update DB.ts

--- src/DB.ts
+++ src/DB.ts
@@ -1,12 +1,1 @@
-const users: User[] = [
-  { id: '1', 
-    username: 'user1', 
-    age: 25, 
-    hobbies: ['drawing', 'running'] 
-  },
-  { id: '2', 
-    username: 'user2', 
-    age: 30, 
-    hobbies: ['cooking', 'coding'] 
-  }
-];
+
