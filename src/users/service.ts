7d925451a97f543085bfb8b426d95f35c52ddb63
malDev09 <159049731+malDev09@users.noreply.github.com>
2024-02-09 16:40:28 +0300

    feat: update service methods

--- src/users/service.ts
+++ src/users/service.ts
@@ -1,13 +1,30 @@
-import { UsersRepository } from "./repository";
+import { IncomingMessage } from 'http';
+import { getBody } from '../helpers';
+import { UsersRepository } from './repository';
 
 export class UsersService {
-  constructor(private usersRepository: UsersRepository){}
+    constructor(private usersRepository: UsersRepository) {}
 
-  async getAll() {
-    return this.usersRepository.getAll();
+    async getAll(): Promise<User[]> {
+        return this.usersRepository.getAll();
+    }
+
+    async getUserById(id: User["id"]): Promise<User | undefined> {
+        return this.usersRepository.getById(id);
+    }
+
+    async createUser(newUser: User): Promise<User> {
+        return this.usersRepository.create(newUser);
+    }
+    async updateUser(userId: string, req: IncomingMessage): Promise<User> {
+      const body = await getBody(req);
+      const updatedUser = await this.usersRepository.update(userId, body);
+      return updatedUser;
   }
 
-  async getUserById(id: User["id"]) {
-    return this.usersRepository.getById(id)
+  async deleteUser(userId: string): Promise<void> {
+      await this.usersRepository.delete(userId);
   }
+
 }
+
