634fecc7d6ffa5ad205376646e7f7e5c96f864b3
malDev09 <159049731+malDev09@users.noreply.github.com>
2024-02-09 16:38:41 +0300

    feat: update router

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,30 +1,44 @@
 import { IncomingMessage, ServerResponse } from 'http';
+import { sendErrorResponse } from '../helpers';
 import { UsersController } from '../users/controller';
 import { UsersRepository } from '../users/repository';
 import { UsersService } from '../users/service';
 
+const usersRepository = new UsersRepository([])
+const usersService = new UsersService(usersRepository)
+const usersController = new UsersController(usersService)
+
 export const router = (req: IncomingMessage, res: ServerResponse) => {
-  const usersRepository = new UsersRepository([])
-  const usersService = new UsersService(usersRepository)
-  const usersController = new UsersController(usersService)
+
     const { url, method } = req;
+    const URL = /^\/api\/users\/?$/;
+    const URL_ID = /^\/api\/users\/[^\/]+$/;
 
     switch (method) {
         case 'GET':
-            if (url === '/api/users') {
+            if (url?.match(URL)) {
                 usersController.getAllUsers(req, res);
-            } else if( url === 'api/users/{userId}') {
-              
+            } 
+             if( url?.match(URL_ID)) {
+              usersController.getUserById(req, res)
             }
             break;
-        // case 'POST':
-        //     if (url === '/api/users') {
-        //         usersController.createUser(req, res);
-        //     } else {
-        //     }
-        //     break;
+        case 'POST':
+            if (url?.match(URL)) {
+                usersController.createUser(req, res);
+            } 
+            break;
+        case 'PUT':
+              if (url?.match(URL_ID)) {
+                  usersController.updateUser(req, res);
+              }
+              break;
+        case 'DELETE':
+              if (url?.match(URL_ID)) {
+                  usersController.deleteUser(req, res);
+              }
+              break;
         default:
-            res.writeHead(404, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'Not Found' }));
+          sendErrorResponse(res, 404, 'Page not found')
     }
 };
