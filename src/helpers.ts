1a1a54a40e9d09f26f04cfc961256973606fc0e8
malDev09 <159049731+malDev09@users.noreply.github.com>
2024-02-11 16:59:38 +0300

    feat: update helpers

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,5 @@
 import { IncomingMessage, ServerResponse } from "http";
+import { User } from "./types";
 
 export function sendErrorResponse(res: ServerResponse, statusCode: number, message: string) {
   res.writeHead(statusCode, { 'Content-Type': 'application/json' });
@@ -26,6 +27,28 @@ export function getBody(req: IncomingMessage): Promise<User> {
       });
   });
 }
+
+export   function validateUser(user: User) {
+  const errors = [];
+  if (!user.username || typeof user.username !== 'string') {
+      errors.push('Username is required and must be a string');
+  }
+
+  if (!user.age || typeof user.age !== 'number' || user.age <= 0) {
+      errors.push('Age is required and must be a positive number');
+  }
+
+  if (!user.hobbies || !Array.isArray(user.hobbies) || user.hobbies.length === 0) {
+      errors.push('Hobbies is required and must be a non-empty array');
+  }
+  if (errors.length === 0) {
+      return  true ;
+  } else {
+      return false;
+  }
+}
+
+
 
 
 export const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
