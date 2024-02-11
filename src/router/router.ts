2da638fdd941decf2b1f771adc5f60e0ef428c07
malDev09 <159049731+malDev09@users.noreply.github.com>
2024-02-11 17:29:17 +0300

    fix: fix router deps

--- src/router/router.ts
+++ src/router/router.ts
@@ -4,23 +4,21 @@ import { UsersController } from '../users/controller';
 import { UsersRepository } from '../users/repository';
 import { UsersService } from '../users/service';
 
-const usersRepository = new UsersRepository([])
-const usersService = new UsersService(usersRepository)
-const usersController = new UsersController(usersService)
-
-export const router = (req: IncomingMessage, res: ServerResponse) => {
-
+export const router = (usersRepository: UsersRepository) => (req: IncomingMessage, res: ServerResponse) => {
     const { url, method } = req;
     const URL = /^\/api\/users\/?$/;
     const URL_ID = /^\/api\/users\/[^\/]+$/;
+
+    const usersService = new UsersService(usersRepository);
+    const usersController = new UsersController(usersService);
 
     switch (method) {
         case 'GET':
             if (url?.match(URL)) {
                 usersController.getAllUsers(req, res);
             } 
-             if( url?.match(URL_ID)) {
-              usersController.getUserById(req, res)
+            if (url?.match(URL_ID)) {
+                usersController.getUserById(req, res);
             }
             break;
         case 'POST':
@@ -29,16 +27,16 @@ export const router = (req: IncomingMessage, res: ServerResponse) => {
             } 
             break;
         case 'PUT':
-              if (url?.match(URL_ID)) {
-                  usersController.updateUser(req, res);
-              }
-              break;
+            if (url?.match(URL_ID)) {
+                usersController.updateUser(req, res);
+            }
+            break;
         case 'DELETE':
-              if (url?.match(URL_ID)) {
-                  usersController.deleteUser(req, res);
-              }
-              break;
+            if (url?.match(URL_ID)) {
+                usersController.deleteUser(req, res);
+            }
+            break;
         default:
-          sendErrorResponse(res, 404, 'Page not found')
+            sendErrorResponse(res, 404, 'Page not found');
     }
 };
